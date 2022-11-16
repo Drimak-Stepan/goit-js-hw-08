6605b8abb8294582733c01f37ea1b802229e8b43
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-16 12:49:55 +0200

    f

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const refDivGallery = document.querySelector(".gallery");
+const newGalleryMarkup = createMarkup(galleryItems); 
 
-console.log(galleryItems);
+function createMarkup(gallery){
+    return gallery.map(({preview, original,description}) => {
+        return `
+            <li>
+                <a class="gallery__item" href="${original}"">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                    />
+                </a>
+            </li>`;}).join("");};
+
+refDivGallery.innerHTML = newGalleryMarkup;
+var lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,});
+
+// console.log(galleryItems);
